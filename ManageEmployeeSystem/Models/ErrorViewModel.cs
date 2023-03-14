84784173a57de65703eaea19cf3013055725b6c0
Guivira <guillermo.ramirezvillalta@ucr.ac.cr>
2023-03-13 21:00:46 -0600

    Initial project state: In this version only the default files from dotnet was submitted and an test model file to test the dotnet sql version

---
+++ ManageEmployeeSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ManageEmployeeSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
